c444621f4fa3adaa56c089ae37e2a3ddea3bdddd
JhonFy <moesdeveloper1023@gmail.com>
2025-10-16 11:37:59 -0500

    [CREAR].[CapaDatos/Metodos]

--- CapaDatos/CodigoDeBarras.cs
+++ CapaDatos/CodigoDeBarras.cs
@@ -8,5 +8,17 @@ namespace CapaDatos
 {
     internal class CodigoDeBarras
     {
+    
+        public int IdIdentity { get; set;}
+        public long Radicado { get; set;}
+        public long Id { get; set;}
+        public string Empleado { get; set;}
+        public string Identificacion { get; set;}
+        public string Tipo_Documental { get; set; }
+        public string Codigo_De_Barras_Recepcion {get; set;}
+        public string CB_Documento { get; set;}
+        public string CB_Expediente { get; set;}
+        public string CB_Caja { get; set;}
+
     }
 }
