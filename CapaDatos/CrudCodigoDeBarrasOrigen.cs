ad9c074c6d688ec112360c3783a070058dfe97fc
JhonFy <moesdeveloper1023@gmail.com>
2025-10-19 04:38:26 -0500

    [MODIFIED].[Conexion.cs].[Agregar Metodo ObtenerConexion]

--- CapaDatos/CrudCodigoDeBarrasOrigen.cs
+++ CapaDatos/CrudCodigoDeBarrasOrigen.cs
@@ -8,18 +8,21 @@ namespace CapaDatos
 {
     public class CrudCodigoDeBarrasOrigen
     {
-        //DB
         private Conexion conexion = new Conexion();
-
-        //READ
+                
         public List<ModeloCodigoDeBarrasOrigen> LeerOrigen() { 
         
             var lista = new List<ModeloCodigoDeBarrasOrigen>();
-
+            
             try
             {
+                
 
-            }catch (Exception ex)
+
+
+
+            }
+            catch (Exception ex)
             {
                 throw new Exception("Error al leer los orígenes de código de barras: " + ex.Message);
             }
