edec211de55aab7996e96ce8afdc6b82f9f00165
JhonFy <moesdeveloper1023@gmail.com>
2025-10-18 23:37:26 -0500

    [CREATE].[ModeloCodigoDeBarrasDestino]

--- CapaDatos/ModeloCodigoDeBarrasDestino.cs
+++ CapaDatos/ModeloCodigoDeBarrasDestino.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace CapaDatos
 {
-    internal class modeloCodigoDeBarrasDestino
-    {
-    }
+    public class ModeloCodigoDeBarrasDestino
+    {   
+        public int IdIdentity { get; set; }
+        public long Radicado { get; set; }
+        public long Id { get; set;  }
+        public string Empleado { get; set; }
+        public string Identificacion { get; set; }
+        public string TipoDocumental { get; set; }
+        public string CodigoDeBarrasRecepcion { get; set; }
+        public string CbDocumento { get; set; }
+        public string CbExpediente { get; set; }
+        public string CbCaja { get; set; }
+
+    }   
 }
