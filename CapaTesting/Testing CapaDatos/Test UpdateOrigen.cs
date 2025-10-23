619609cb65d544ba93f77fffc4c8f0e85cf3731b
JhonFy <moesdeveloper1023@gmail.com>
2025-10-23 10:32:30 -0500

    [CREATE].[Capa Testing].[Se Crea ÃLa Clase Test UpdateOrigen]

---
+++ CapaTesting/Testing CapaDatos/Test UpdateOrigen.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaTesting.Testing_CapaDatos
+{
+    internal class Test_UpdateOrigen
+    {
+    }
+}
