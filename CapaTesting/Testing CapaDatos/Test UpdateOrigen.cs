577aa8acee3614afe44b5e5680dfa471e8129b72
JhonFy <moesdeveloper1023@gmail.com>
2025-10-23 11:18:59 -0500

    [MODIFIED].[CapaDatos].[Actualizar Metodo UpdateOrigen V2]

--- CapaTesting/Testing CapaDatos/Test UpdateOrigen.cs
+++ CapaTesting/Testing CapaDatos/Test UpdateOrigen.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CapaDatos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,7 +15,10 @@ namespace CapaTesting.Testing_CapaDatos
         {
             try
             {
+                ModeloCodigoDeBarrasOrigen objUpdateOrigen = new ModeloCodigoDeBarrasOrigen
+                {
 
+                };
             }
             catch (Exception ex)
             {
