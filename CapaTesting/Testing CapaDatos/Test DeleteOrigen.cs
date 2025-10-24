c29a8ccbabee77f1b630ea888e2f8568d1a9caa6
JhonFy <moesdeveloper1023@gmail.com>
2025-10-24 03:25:38 -0500

    [MODIFIED].[CapaDatos].[Se Crea El Metodo DeleteOrigen]

--- CapaTesting/Testing CapaDatos/Test DeleteOrigen.cs
+++ CapaTesting/Testing CapaDatos/Test DeleteOrigen.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CapaDatos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,31 @@ using System.Threading.Tasks;
 
 namespace CapaTesting.Testing_CapaDatos
 {
-    public class Test_DeleteOrigen
+    [TestClass]
+    public class Test_DeleteOrigen()
     {
 
+        [TestMethod]
+        public void TestDeleteOrigen()
+        {
+            try
+            {
+                ModeloCodigoDeBarrasOrigen borrarRegistro = new ModeloCodigoDeBarrasOrigen
+                {
+                    Identificacion = "9876543210"
+                };
+
+                CrudCodigoDeBarrasOrigen objDelete = new CrudCodigoDeBarrasOrigen();
+                bool resultado = objDelete.DeleteOrigen(borrarRegistro);
+
+                Assert.IsTrue(resultado, "[Test Delete Origen] No se eliminó ningún registro.");
+                Console.WriteLine("[Test Delete Origen] El registro fue eliminado correctamente.");
+
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail($"[Test Delete Origen] La prueba falló con una excepción: {ex.Message}");
+            }
+        }
     }
 }
