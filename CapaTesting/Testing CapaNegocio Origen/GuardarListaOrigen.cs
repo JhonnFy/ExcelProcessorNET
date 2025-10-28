3fb104773e330ada0310015a15a230ec84e16b5a
JhonFy <moesdeveloper1023@gmail.com>
2025-10-28 01:56:23 -0500

    [CREATE].[CapaTesting].[TestCapaNegocioOrigenCrear]

---
+++ CapaTesting/Testing CapaNegocio Origen/GuardarListaOrigen.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDatos;
+
+namespace CapaTesting.Testing_CapaNegocio_Origen
+{
+    [TestClass]
+    public class GuardarListaOrigen
+    {
+        [TestMethod]
+        public void TestCapaNegocioOrigenCrear()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail($"[TestCapaNegocioOrigenCrear] La prueba fallo con una excepción: {ex.Message}");
+            }
+        }
+    }
+}
