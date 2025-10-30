9cb96d9593b21bcc7eefa721b9e13dc7e164f8ee
JhonFy <moesdeveloper1023@gmail.com>
2025-10-30 12:23:00 -0500

    [CREATE].[CapaIgu].[ConstruirBtnImport]

--- CapaIgu/AppRun.cs
+++ CapaIgu/AppRun.cs
@@ -35,6 +35,7 @@ namespace CapaIgu
             this.BeginInvoke((Action)(() => this.ActiveControl = null));
             ConfigurarDataGridView();
             ConstruirBtnCreate();
+            ConstruirBtnImport();
         }
 
 
@@ -46,32 +47,39 @@ namespace CapaIgu
 
         private void ConfigurarDataGridView()
         {
-            dataGridViewExcel.Columns.Clear();
-            dataGridViewExcel.AutoGenerateColumns = false;
-            dataGridViewExcel.AllowUserToAddRows = false;
-            dataGridViewExcel.ReadOnly = true;
-            dataGridViewExcel.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-
-            dataGridViewExcel.Columns.Add("Radicado", "Radicado");
-            dataGridViewExcel.Columns.Add("Id", "Id");
-            dataGridViewExcel.Columns.Add("Empleado", "Empleado");
-            dataGridViewExcel.Columns.Add("Identificacion", "Identificacion");
-            dataGridViewExcel.Columns.Add("Tipo Documental", "Tipo Documental");
-            dataGridViewExcel.Columns.Add("Codigo De Barras", "Codigo De Barras");
-            dataGridViewExcel.Columns.Add("Cb Documento", "Cb Documento");
-            dataGridViewExcel.Columns.Add("CB Expediente", "CB Expediente");
-            dataGridViewExcel.Columns.Add("CB Caja", "CB Caja");
-            dataGridViewExcel.Columns.Add("Btn_IMPORT", "");
-            dataGridViewExcel.Columns.Add("Btn_CREATE", "");
-            dataGridViewExcel.RowHeadersVisible = false;
-            dataGridViewExcel.AllowUserToResizeColumns = false;
-            dataGridViewExcel.ScrollBars = ScrollBars.Vertical;
-            dataGridViewExcel.BackgroundColor = Color.White;
-
-            foreach (DataGridViewColumn column in dataGridViewExcel.Columns)
+            try
             {
-                column.SortMode = DataGridViewColumnSortMode.NotSortable;
-                column.Width = 124;
+                dataGridViewExcel.Columns.Clear();
+                dataGridViewExcel.AutoGenerateColumns = false;
+                dataGridViewExcel.AllowUserToAddRows = false;
+                dataGridViewExcel.ReadOnly = true;
+                dataGridViewExcel.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+                dataGridViewExcel.Columns.Add("Radicado", "Radicado");
+                dataGridViewExcel.Columns.Add("Id", "Id");
+                dataGridViewExcel.Columns.Add("Empleado", "Empleado");
+                dataGridViewExcel.Columns.Add("Identificacion", "Identificacion");
+                dataGridViewExcel.Columns.Add("Tipo Documental", "Tipo Documental");
+                dataGridViewExcel.Columns.Add("Codigo De Barras", "Codigo De Barras");
+                dataGridViewExcel.Columns.Add("Cb Documento", "Cb Documento");
+                dataGridViewExcel.Columns.Add("CB Expediente", "CB Expediente");
+                dataGridViewExcel.Columns.Add("CB Caja", "CB Caja");
+                dataGridViewExcel.Columns.Add("Btn_IMPORT", "");
+                dataGridViewExcel.Columns.Add("Btn_CREATE", "");
+                dataGridViewExcel.RowHeadersVisible = false;
+                dataGridViewExcel.AllowUserToResizeColumns = false;
+                dataGridViewExcel.ScrollBars = ScrollBars.Vertical;
+                dataGridViewExcel.BackgroundColor = Color.White;
+
+                foreach (DataGridViewColumn column in dataGridViewExcel.Columns)
+                {
+                    column.SortMode = DataGridViewColumnSortMode.NotSortable;
+                    column.Width = 124;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error En El DataGridView " + ex.Message);
             }
 
         }
@@ -128,6 +136,42 @@ namespace CapaIgu
         {
             try
             {
+                Button objBtnImport = new Button();
+
+                objBtnImport.Size = new Size(123, 21);
+                objBtnImport.FlatStyle = FlatStyle.Flat;
+                objBtnImport.UseVisualStyleBackColor = false;
+                objBtnImport.FlatAppearance.BorderSize = 0;
+                objBtnImport.FlatAppearance.MouseOverBackColor = Color.White;
+                objBtnImport.FlatAppearance.MouseDownBackColor = Color.White;
+
+                string rutaImagen = @"C:\Developer\ExcelProcessorNET\Icons\btnImport.jpg";
+
+                if (System.IO.File.Exists(rutaImagen))
+                {
+                    Image original = Image.FromFile(rutaImagen);
+
+                    objBtnImport.BackgroundImage = new Bitmap(original, new Size(40, 20));
+                    objBtnImport.BackgroundImageLayout = ImageLayout.Center;
+                    objBtnImport.ImageAlign = ContentAlignment.MiddleCenter;
+                }
+                else
+                {
+                    MessageBox.Show("La imagen no se encontró en la ruta especificada: " + rutaImagen);
+                }
+
+                int margenDerecha = 125 + 5; // ancho del botón Create (123) + pequeño espacio
+                int margenArriba = 2;
+
+                objBtnImport.Location = new Point(
+                    this.ClientSize.Width - objBtnImport.Width - margenDerecha,
+                    margenArriba
+                );
+
+                objBtnImport.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+                this.Controls.Add(objBtnImport);
+                objBtnImport.BringToFront();
+
             }
             catch (Exception ex)
             {
